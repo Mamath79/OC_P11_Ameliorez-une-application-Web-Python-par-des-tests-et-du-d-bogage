70f1030e7d592b232bacf7b440c01802ae288886
Mathieu Vieillefont <57585607+Mamath79@users.noreply.github.com>
2025-01-30 12:43:17 +0100

    fix issue , cannot book more places than availables

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import json
 from flask import Flask,render_template,request,redirect,flash,url_for
+from utilities.cannot_book_more_places_than_availables import cannot_book_more_places_than_availables
 
 
 def loadClubs():
@@ -46,6 +47,11 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+
+    if not cannot_book_more_places_than_availables(placesRequired,competition):
+        flash('You can not book more places than availables')
+        return render_template('welcome.html', club=club, competitions=competitions)
+
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
